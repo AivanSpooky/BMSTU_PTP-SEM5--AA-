75227696b932106e0fa2a661646a473ca73c41e2
AivanSpooky <IvanSmirnov2002@yandex.ru>
2024-10-21 00:50:59 +0300

    Revert "Revert "HAHAHHAH""
    This reverts commit c124e6fbd234d21121e3dbdd0b03e37e9d222dfc.

---
+++ timestamp_cas/timestamp_cas/timestamp_cas/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace timestamp_cas
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            DateTimeOffset currentTime = DateTimeOffset.Now;
+            string formattedTime = currentTime.ToString("yyyy-MM-ddTHH:mm:sszzz");
+            Console.WriteLine($"\"{formattedTime}\"");
+        }
+    }
+}
